0bca3d22c735aec40cef73fe6445ede83e2a1bf2
Oukey <oukey311@gmail.com>
2019-10-25 20:21:19 +0400

    Работа с пакетами и файлами. Добавлен блок исключений

--- Practical_course/2_Work_with_files/calculation.py
+++ Practical_course/2_Work_with_files/calculation.py
@@ -9,20 +9,23 @@ def sum_file(name):
     sum = 0
     check = True
     lines = 0
-    with open(str(name) + '.txt', 'rt') as file:
-        line = file.readline()
-        while line != '':
-            try:
-                sum += int(line.rstrip())
-                lines += 1
-                line = file.readline()
-            except ValueError:
-                check = False
-                break
-        if lines == 3 and check is True:
-            return sum
-        else:
-            print("В файле '{}.txt' не корректные данные".format(name))
+    try:
+        with open(str(name) + '.txt', 'rt') as file:
+            line = file.readline()
+            while line != '':
+                try:
+                    sum += int(line.rstrip())
+                    lines += 1
+                    line = file.readline()
+                except ValueError:
+                    check = False
+                    break
+            if lines == 3 and check is True:
+                return sum
+            else:
+                print("В файле '{}.txt' не корректные данные!".format(name))
+    except FileNotFoundError:
+        print("Файл с именем '{}.txt' - недоступен!".format(name))
 
 
 def res_files(n, m):
@@ -30,17 +33,15 @@ def res_files(n, m):
     Функция получает два числа от 1 до 10, открывает одноименные файлы
     И возвращает сумму чисел в двух файлах
     '''
-    try:
+    if sum_file(n) and sum_file(m):
+        # output_info()
         return sum_file(n) + sum_file(m)
-    except TypeError:
-        print('Не корректные данные')
+    print('Получены не корректные данные!')
 
 
-rand_num_1 = 11
-# rand_num_1 = random.randint(1, 10)
-rand_num_2 = 12
-# rand_num_2 = random.randint(1, 10)
+rand_num_1 = random.randint(1, 10)
+rand_num_2 = random.randint(1, 10)
+
 print("Сумма чисел в файле '{}.txt' == {}".format(rand_num_1, sum_file(rand_num_1)))
 print("Сумма чисел в файле '{}.txt' == {}".format(rand_num_2, sum_file(rand_num_2)))
 print("Сумма чисел в двух выбранных файлах == {}".format(res_files(rand_num_1, rand_num_2)))
-
