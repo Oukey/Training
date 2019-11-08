77f82bf1dcf9768cdb3741980eaff6c9fe309a31
Oukey <oukey311@gmail.com>
2019-11-08 22:36:35 +0400

    Работа с XML 2. Продолжение

--- Practical_course/4_Work_with_XML/tasks_two.py
+++ Practical_course/4_Work_with_XML/tasks_two.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # 1. Написать функциюб которая формирует списко всех узлов по заданному тегу
 # независимо от их глубины в XML-документе. На вход функция получает корневой узел
 
@@ -14,76 +16,33 @@ def get_root(xml_file="demo.xml"):
     return xml2.getroot()
 
 
-def get_list_nodes(rt, tag):
-    '''
+def get_list_nodes(root, tag):
+    """
     Функция формирует и возвращает список всех узлов по заданному тегу
     Функция получает корневой узел
-    '''
+    """
     list_node = []
-    for nod in rt.iter():
+    for nod in root.iter():
         if nod.tag == tag:
             list_node.append(nod)
-            # print(nod.tag)
     return list_node
 
 
-def parent_search(xml_file, node=None):
-    '''Функция поиска родителя разанного узла'''
+def parent_search(xml_file, tag):
+    """Функция поиска родителя разанного узла"""
     root = get_root(xml_file)
-    for item in root.iter():
-        print(item.tag)
+    for elem in root.iter():
+        if elem.findall(tag):
+            return elem
 
 
 def remove_nodes(xml_file, tag):
-    '''Функция удаления всех узлов по заданному тегу'''
-    root = get_root(xml_file)
-    for elem in root.iter():
-        item = elem.find(tag)
-        elem.remove(item)
+    """ Функция удаления всех узлов по заданному тегу """
+    pass
 
 
+# def output_elem(xml_file):
+#     root = get_root(xml_file)
+#     for elem in root.iter():
+#         print(elem.tag)
 
-'''
-xml_file = 'demo.xml'
-xml2 = ETree.parse(xml_file)
-root = xml2.getroot()
-for elem in root.iter():
-    # print(elem.tag, elem.text, elem.attrib)
-    print(elem.tag)
-
-tag = 'data'
-rt = xml2.getroot()
-for elem in root.iter():
-    item = elem.find(tag)
-    if item:
-        elem.remove(item)
-    # if elem.find(tag):
-    #     elem.remove(elem.find(tag))
-
-print('*' * 40)
-for elem in rt.iter():
-    # print(elem.tag, elem.text, elem.attrib)
-    print(elem.tag)
-
-print('*' * 40)
-zz = root.find('languages')  #languages
-print(zz)
-
-
-
-
-# print(root.tag)
-#
-# print(get_list_nodes(root, 'name'))
-# print(get_list_nodes(root, 'languages'))
-# print(get_list_nodes(root, 'language'))
-# print('*' * 40)
-# l = get_list_nodes(root, 'language')
-# l1 = l[1]
-# print(type(l1))
-#
-#
-# print('*' * 40)
-# print(parent_search(xml_file))
-
-'''
