f7bac4fc9346ce8b5ae9e594d24742ab3ee71fc1
Oukey <oukey311@gmail.com>
2019-12-11 22:27:47 +0400

    Задание по работе с doc файлом, поиск строки в таблице

--- Practical_course/6_Work_with_docs/Task_8.4.1.py
+++ Practical_course/6_Work_with_docs/Task_8.4.1.py
@@ -9,24 +9,24 @@ from docx import Document
 def string_replacement(file_name, str1, str2):
     """
     Функция получает на вход имя файла(документ Word) и две строки.
-    Выполняет замену одной строки на другую.
+    Выполняет замену первой найденной строки на другую.
     При успешной замене возвращает True, иначе False
     """
+
     if os.path.isfile(file_name):
         document = Document(file_name)
+        # Поиск строки в параграфах
         for per in document.paragraphs:
             if per.text == str1:
                 per.text = str2
                 document.save(file_name)
                 return True
+        # Поиск строки в таблицах
+        for selected_table in document.tables:
+            for selected_row in selected_table.rows:
+                for selected_cell in selected_row.cells:
+                    if selected_cell.text == str1:
+                        selected_cell.text = str2
+                        document.save(file_name)
+                        return True
     return False
-
-
-name_file = 'demo.docx'
-str1 = 'A plan paragraph having some bold and some italic'
-str2 = 'XXX'
-
-
-print(string_replacement(name_file, str1, str2))
-
-
