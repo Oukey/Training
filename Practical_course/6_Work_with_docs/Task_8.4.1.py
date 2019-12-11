6cdf2b382fb5926b3cc1c68609a323311a640f9e
Oukey <oukey311@gmail.com>
2019-12-12 00:28:29 +0400

    Добавлено решение по заполнению excel файла

--- Practical_course/6_Work_with_docs/Task_8.4.1.py
+++ Practical_course/6_Work_with_docs/Task_8.4.1.py
@@ -2,8 +2,9 @@
 """Работа с документами doc"""
 
 import os.path
-
+import random
 from docx import Document
+from openpyxl import Workbook, load_workbook, utils
 
 
 def string_replacement(file_name, str1, str2):
@@ -12,21 +13,50 @@ def string_replacement(file_name, str1, str2):
     Выполняет замену первой найденной строки на другую.
     При успешной замене возвращает True, иначе False
     """
-
+    # Проверка наличия заданного файла
     if os.path.isfile(file_name):
-        document = Document(file_name)
-        # Поиск строки в параграфах
-        for per in document.paragraphs:
-            if per.text == str1:
-                per.text = str2
-                document.save(file_name)
-                return True
-        # Поиск строки в таблицах
-        for selected_table in document.tables:
-            for selected_row in selected_table.rows:
-                for selected_cell in selected_row.cells:
-                    if selected_cell.text == str1:
-                        selected_cell.text = str2
-                        document.save(file_name)
-                        return True
+        try:
+            document = Document(file_name)
+            # Поиск строки в параграфах
+            for per in document.paragraphs:
+                if per.text == str1:
+                    per.text = str2
+                    document.save(file_name)
+                    return True
+            # Поиск строки в таблицах
+            for selected_table in document.tables:
+                for selected_row in selected_table.rows:
+                    for selected_cell in selected_row.cells:
+                        if selected_cell.text == str1:
+                            selected_cell.text = str2
+                            document.save(file_name)
+                            return True
+        except ValueError:
+            print('Неверный формат файла!')
     return False
+
+
+def fil_square(file_name):
+    """
+    Функция открывает excel-файл по заданному имени.
+    На случайном листе заполняет квадрат 12*12 ячеек случайными значениями
+    При успешном выполнении возвращает True иначе False
+    """
+    # Проверка наличия заданного файла
+    if os.path.isfile(file_name):
+        try:
+            excel_file = load_workbook(file_name)
+            # Выбор случайного листа
+            selected_sheet = excel_file[random.choice(excel_file.sheetnames)]
+            # заполнение ячеек случайными  числами
+            for x in range(1, 13):
+                for y in range(1, 13):
+                    selected_sheet.cell(row=x, column=y, value=random.randint(0, 101))
+            excel_file.save(filename=file_name)
+            return True
+        # except ValueError:
+        except utils.exceptions.InvalidFileException:
+            print('Не поддерживается формат файла!')
+    return False
+
+
